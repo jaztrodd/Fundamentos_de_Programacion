658cfa8c89eb5f85790da65255bbb8f78d73d2d3
jaztrodd <L24550361@chihuahua2.tecnm.mx>
2024-08-28 23:52:21 -0600

    EVA1_3_AUTOMOVIL.java

---
+++ EVA1_3_AUTOMOVIL.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_3_automovil;
+
+/**
+ *
+ */
+public class EVA1_3_AUTOMOVIL {
+
+    public static void main(String[] args) {
+        //DECLARACIÓN DELAS VARIABLES
+        String marca;
+        String modelo;
+        int year;
+        double precio;
+        
+        //ASIGNAR VALORES
+        marca = "Ford";
+        modelo = "Mustang";
+        year = 2022;
+        precio = 999000.1;
+        
+        System.out.println(marca);//imprime el contenido de la variable
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio);
+    }
+}
