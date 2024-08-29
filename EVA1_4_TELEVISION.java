04bd38061d94a58c387ae695bdf40b82b91c72a6
jaztrodd <L24550361@chihuahua2.tecnm.mx>
2024-08-28 23:51:30 -0600

    EVA1_4_TELEVISION.java

---
+++ EVA1_4_TELEVISION.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_4_television;
+
+/**
+ *
+ */
+public class EVA1_4_TELEVISION {
+
+    public static void main(String[] args) {
+      //DELCARAR VARIABLES
+       String marca;
+       String modelo;
+       int pulgadas;
+       double precio;
+       String accesible;
+       
+      //ASIGNAR VALORES
+      marca = "LG";
+      modelo = "oled" ;
+      pulgadas = 32;
+      precio = 2500.1;
+      accesible = "para todos";
+      
+        System.out.println(marca);//imprime el contenido de la variable
+        System.out.println(modelo);
+        System.out.println(pulgadas);
+        System.out.println(precio);
+        System.out.println(accesible);    
+    }
