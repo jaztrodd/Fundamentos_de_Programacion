c7a2bd657880e027e140a73e7cf125a0340160d6
jaztrodd <L24550361@chihuahua2.tecnm.mx>
2024-08-28 23:54:10 -0600

    EVA1_2_VARIABLES.java

---
+++ EVA1_2_VARIABLES.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_2_variables;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_2_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+                                        //TIPOS DE DATOS
+    System.out.println("HOLA MUNDO!!"); //Cadena De Texto
+    System.out.println(100);            //Un Numero Entero
+    System.out.println('c');            //Un Caracter
+    System.out.println(true);           //Valores Logicos
+    System.out.println(false);
+    System.out.println(1000.52);        //Números Con Fracción
+    
+    //TIPOS DE DATOS
+    //ENTEOS
+    //int
+    //long
+    //byte
+    //short
+    //PUNTO FLOTANTE
+    //double
+    //float
+    //TEXTO
+    //String
+    //CHARACTER
+    //char
+    //LOGICOS (true false)
+    //boolean
+    
+    //creacion y uso de variables 
+    int edad; //declara una variable de tipo entero, llamada edad
+     //double edad2;  //   NO PODEMOS TENER DOS VARIABLES CON EL MISMO NOMBRE 
+     String nombre;
+     String apellido;
+     double promedio;
+     
+     edad = 18;  //estoy asignando el valor 18 dentro de la variable edad
+     System.out.println(edad); //println tamboeben imprime variable edad
+     
+    }
+    
+    
+}
